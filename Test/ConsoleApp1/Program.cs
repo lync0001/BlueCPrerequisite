16eebaf0c2075332615605eb59e88b8044a79b4c
Andries <nieu0082@hz.nl>
2017-11-06 17:19:04 +0100

    Test added.

---
+++ Test/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name..");
+            string name = Console.ReadLine();
+            string approved = "approved";
+
+            string gonogo = $"System is capable of running .NET Core 2.0. {name} joining BlueJ is {approved}, as of {DateTime.Now.ToLocalTime()}!";
+
+            Console.WriteLine(gonogo);
+            Console.Read();
+        }
+    }
+}
