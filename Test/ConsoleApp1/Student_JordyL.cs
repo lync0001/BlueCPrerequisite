04cbec5d4810c18db09b93c3b92b632347760aa3
Jordy <lync0001@hz.nl>
2017-11-13 09:45:58 +0100

    student info

---
+++ Test/ConsoleApp1/Student_JordyL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        public int Student_ID = 00073712;
+        public int Leeftijd = 22;
+        public string Naam = "Jordy Lynch";
+        public string Status_Propedeuse = "Nog niet behaald";
+        public bool Rijbewijs = true;
+        public bool The_Cake = false;
+        public bool Relatie = true; 
+	}
+}
