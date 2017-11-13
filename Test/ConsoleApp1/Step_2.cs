86d651517449c1073c90b0f87c5ea9895f22afe5
Jordy <lync0001@hz.nl>
2017-11-13 10:46:21 +0100

    added constructor and new class

---
+++ Test/ConsoleApp1/Step_2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Step_2
+{
+    private string school;
+    private bool gender;
+    private string name;
+    private int age;
+
+    public Step_2(string name, int age, bool gender)
+    {
+        this.name = "Jordy";
+        this.age = 22;
+        this.gender = 1;
+    }
+
+    public string getName()
+    {
+        return this.name;
+    }
+
+    public int getAge()
+    {
+        return this.age;
+    }
+
+    public bool getGender()
+    {
+        return this.gender;
+    }
+}
