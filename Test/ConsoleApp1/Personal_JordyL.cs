9ec0caa608cf94d4d6e6349a6c3ee52d7cca0963
Jordy <lync0001@hz.nl>
2017-11-13 09:52:21 +0100

    Switched Personal and student info to separete files

---
+++ Test/ConsoleApp1/Personal_JordyL.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        public bool Rijbewijs = true;
+        public bool The_Cake = false;
+        public bool Relatie = true;
+}
+}
--- Test/ConsoleApp1/Student_JordyL.cs
+++ Test/ConsoleApp1/Student_JordyL.cs
@@ -8,8 +8,5 @@ public class Class1
         public int Leeftijd = 22;
         public string Naam = "Jordy Lynch";
         public string Status_Propedeuse = "Nog niet behaald";
-        public bool Rijbewijs = true;
-        public bool The_Cake = false;
-        public bool Relatie = true; 
 	}
 }
